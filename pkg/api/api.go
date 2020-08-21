f5ea20c9edcd48b741b159ff6b91fdc1835795ba
Boris Bokan <boris@bokan.io>
2020-08-21 12:57:53 +0200

    Add server shutdown when context ends

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -16,12 +16,17 @@ type API struct {
 	addr string
 	d    download.Downloader
 	fd   facedetect.FaceDetector
+	srv  http.Server
 }
 
+// NewAPI creates a REST API responsible for serving face detection requests.
+// Call Serve afterwards and pass parent context and return value of Routes
+// as parameters,
 func NewAPI(addr string, d download.Downloader, fd facedetect.FaceDetector) *API {
 	return &API{addr: addr, d: d, fd: fd}
 }
 
+// Routes returns a http.Handler with routes configured.
 func (a *API) Routes() http.Handler {
 	r := mux.NewRouter()
 	r.Methods(http.MethodGet).Path("/v1/face-detect").HandlerFunc(a.handleFaceDetect)
@@ -35,8 +40,10 @@ func (a *API) Routes() http.Handler {
 	return handlers.CORS(headersOk, allowAllOrigins, methodsOk)(r)
 }
 
-func (a *API) Serve(ctx context.Context) error {
-	srv := http.Server{
+// Serve starts a HTTP server and serves provided handler. To invoke face detection
+// endpoint, perform a GET request on /v1/face-detect?={image_url}
+func (a *API) Serve(ctx context.Context, handler http.Handler) error {
+	a.srv = http.Server{
 		Addr:              a.addr,
 		ReadTimeout:       time.Second * 2,
 		ReadHeaderTimeout: time.Second * 2,
@@ -46,9 +53,13 @@ func (a *API) Serve(ctx context.Context) error {
 		BaseContext: func(listener net.Listener) context.Context {
 			return ctx
 		},
-		Handler: a.Routes(),
+		Handler: handler,
 	}
-	if err := srv.ListenAndServe(); err != nil {
+	go func() {
+		<-ctx.Done()
+		_ = a.srv.Shutdown(ctx)
+	}()
+	if err := a.srv.ListenAndServe(); err != nil {
 		return err
 	}
 	return nil

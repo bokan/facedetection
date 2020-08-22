926e5e4d59314c896ec2499db3ab322350e16bbf
Boris Bokan <boris@bokan.io>
2020-08-22 15:11:35 +0200

    Refactor main package

--- cmd/facedetection/facedetection.go
+++ cmd/facedetection/facedetection.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
+	"syscall"
 	"time"
 
 	"github.com/bokan/facedetection/pkg/api"
@@ -24,57 +23,56 @@ const (
 func run(ctx context.Context, args []string) error {
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
 	var (
-		port         = flags.Int("p", 8000, "-p <listen_port>")
-		cascadesPath = flags.String("c", "pkg/facedetect/pigofacedetect/cascades", "-c <cascades_path>")
+		port         = flags.Int("p", 8000, "specify the listen port")
+		cascadesPath = flags.String("c", "pkg/facedetect/pigofacedetect/cascades", "specifies the cascades path")
 	)
 	if err := flags.Parse(args[1:]); err != nil {
 		return err
 	}
 
-	logger, err := zap.NewDevelopment()
+	log, err := initLogger()
 	if err != nil {
 		return err
 	}
-	sugar := logger.Sugar()
 
 	d := httpdownloader.NewHTTPDownloader(time.Second*5, 10*1024*1024)
 	fd := pigofacedetect.NewPigoFaceDetector()
 	if err := fd.LoadCascades(*cascadesPath); err != nil {
-		sugar.Fatalw("PigoFaceDetector was unable to load cascades, provide cascade dir with -c flag", "dir", *cascadesPath)
+		log.Fatalw("PigoFaceDetector was unable to load cascades, provide cascade dir with -c flag", "dir", *cascadesPath)
 		return err
 	}
 	a := api.NewAPI(fmt.Sprintf(":%d", *port), d, fd)
 
-	sugar.Infow("Starting service", "port", *port)
+	log.Infow("Starting service", "port", *port)
 	if err := a.Serve(ctx, a.Routes()); err != nil {
 		if err == http.ErrServerClosed {
-			sugar.Warn("Context ended, server stopped.")
+			log.Warn("Context ended, server stopped.")
 			return nil
 		}
-		sugar.Errorw("Serve() error", "err", err)
+		log.Errorw("Serve() error", "err", err)
 		return err
 	}
 	return nil
 }
 
-func main() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+func initLogger() (*zap.SugaredLogger, error) {
+	logger, err := zap.NewDevelopment()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Println(dir)
+	sugar := logger.Sugar()
+	return sugar, nil
+}
+
+func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-	defer func() {
-		signal.Stop(sigCh)
-		cancel()
-	}()
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
 		select {
-		case <-sigCh:
+		case <-sigc:
 			cancel()
 		case <-ctx.Done():
 		}
@@ -84,4 +82,8 @@ func main() {
 		os.Exit(ExitCodeError)
 	}
 
+	select {
+	case <-sigc:
+	case <-time.After(time.Second):
+	}
 }

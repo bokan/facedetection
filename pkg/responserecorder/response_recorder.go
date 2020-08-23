7635c503fb14800bb0c23930530d5a7dbc255ca2
Boris Bokan <boris@bokan.io>
2020-08-23 07:15:38 +0200

    Add comments

--- pkg/responserecorder/response_recorder.go
+++ pkg/responserecorder/response_recorder.go
@@ -12,22 +12,27 @@ type ResponseRecorder struct {
 	statusCode int
 }
 
-func (c *ResponseRecorder) Body() []byte {
-	return c.buf.Bytes()
-}
-
-func (c *ResponseRecorder) StatusCode() int {
-	return c.statusCode
-}
-
+// NewResponseRecorder returns a new instance of write-through response recorder.
 func NewResponseRecorder(w http.ResponseWriter, r *http.Request) *ResponseRecorder {
 	return &ResponseRecorder{w: w, r: r}
 }
 
+// Body returns body content of recorded response.
+func (c *ResponseRecorder) Body() []byte {
+	return c.buf.Bytes()
+}
+
+// StatusCode returns status code of recorded response.
+func (c *ResponseRecorder) StatusCode() int {
+	return c.statusCode
+}
+
+// Header returns header of recorded response.
 func (c *ResponseRecorder) Header() http.Header {
 	return c.w.Header()
 }
 
+// Write should be called inside the HTTP handler.
 func (c *ResponseRecorder) Write(bytes []byte) (int, error) {
 	if c.buf == nil {
 		panic("Write() called before WriteHeader()")
@@ -36,6 +41,7 @@ func (c *ResponseRecorder) Write(bytes []byte) (int, error) {
 	return c.w.Write(bytes)
 }
 
+// WriteHeader should be called inside the HTTP handler.
 func (c *ResponseRecorder) WriteHeader(statusCode int) {
 	c.statusCode = statusCode
 	c.w.WriteHeader(statusCode)
